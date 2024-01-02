9f2f3d1049b80345168dab012032999f70683edd
muhammadikbarasapa <muhammadikbarasapa@smkwikrama.sch.id>
2024-01-02 13:29:19 +0700

    flowchart

---
+++ nilai.js
@@ -0,0 +1,57 @@
+// jp = 1;
+
+// while (jp =10){
+//     document.write ("<h1>masih belajar dikelas</h1>") 
+// }
+// for(let i = 40; i>0; i--){
+//     document.write('<p> hallow word</p>')
+// }
+// i =40;
+// while(i >=1){
+//     document.write(i)
+//     i--;
+// }
+x =50;
+do{
+    document.write( x +'<P>semangat produktif</p>')
+    //iterasi decrement
+    x--;
+    //kondisi perhentian dimasukan didalam while
+} while(x>=0)
+
+
+
+
+
+
+
+// for(let i = 10; i> 0; i--) {
+//     // console.log('mesin nyala')
+//     document.write('<p>mesin nyala</p>')
+// }
+// let a = '80';
+// let b = '90';
+// console.log(a + b);
+// let C = true;
+// console.log(typeof (C))
+
+// let pabp = 70 ;
+// let pplg = 82 ;
+// let pp = 77 ;
+// let rata2 = (pabp + pplg + pp ) /3 ;
+
+// if (rata2 >= 80 && rata2 <= 100 ) {
+//     grade = "A";
+// } else if (rata2 >= 70 && rata2 <=79 ) {
+//     grade = "B";
+// } else if (rata2 >= 90 && rata2 <=70) {
+//     grade = "C";
+// } else if (rata2 >= 45 && rata2 <= 64) {
+//     grade = "D";
+// } else if (rata2 >= 0 && rata2 <= 44) {
+//     grade = "E"
+// } else {
+//     grade = "K"
+// }
+// console.log(rata2)
+// console.log(grade)
